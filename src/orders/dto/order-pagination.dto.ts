e787a41b89f1653a8bc8fcbe4044fd4524d3f6c6
Marco Alvarado <hola@7pixel.cl>
2024-06-17 17:33:41 -0400

    update for orders microservice

---
+++ src/orders/dto/order-pagination.dto.ts
@@ -0,0 +1,11 @@
+import { IsEnum, IsOptional } from 'class-validator';
+import { OrderStatus, OrderStatusList } from '../enum/order.enum';
+import { PaginationDto } from 'src/common';
+
+export class OrderPaginationDto extends PaginationDto {
+  @IsOptional()
+  @IsEnum(OrderStatusList, {
+    message: `Possible status values are ${OrderStatusList}`,
+  })
+  status: OrderStatus;
+}
---
+++ src/orders/orders.controller.ts
@@ -0,0 +1,79 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Inject,
+  Query,
+  ParseUUIDPipe,
+  Patch,
+} from '@nestjs/common';
+import { ORDER_SERVICE } from 'src/config';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
+import { firstValueFrom } from 'rxjs';
+import { CreateOrderDto, OrderPaginationDto } from './dto';
+import { PaginationDto } from 'src/common';
+import { StatusDto } from './dto/status.dto';
+
+@Controller('orders')
+export class OrdersController {
+  constructor(
+    @Inject(ORDER_SERVICE) private readonly ordersClient: ClientProxy,
+  ) {}
+
+  @Post()
+  createOrder(@Body() createOrderDto: CreateOrderDto) {
+    return this.ordersClient.send('createOrder', createOrderDto);
+  }
+
+  @Get()
+  getAllOrders(@Query() orderPaginationDto: OrderPaginationDto) {
+    return this.ordersClient.send('findAllOrders', orderPaginationDto);
+  }
+
+  @Get('id/:id')
+  async getOrder(@Param('id', ParseUUIDPipe) id: string) {
+    try {
+      const order = await firstValueFrom(
+        this.ordersClient.send('findOneOrder', { id }),
+      );
+      if (!order) {
+        throw new RpcException('Order not found');
+      }
+    } catch (error) {
+      throw new RpcException(error);
+    }
+    return this.ordersClient.send('findOneOrder', { id });
+  }
+
+  @Get(':status')
+  async getOrderByStatus(
+    @Param() statusDto: StatusDto,
+    @Query() paginationDto: PaginationDto,
+  ) {
+    try {
+      return this.ordersClient.send('findAllOrders', {
+        ...paginationDto,
+        status: statusDto.status,
+      });
+    } catch (error) {
+      throw new RpcException(error);
+    }
+  }
+
+  @Patch(':id')
+  updateOrderStatus(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() statusDto: StatusDto,
+  ) {
+    try {
+      return this.ordersClient.send('changeOrderStatus', {
+        id,
+        status: statusDto.status,
+      });
+    } catch (error) {
+      throw new RpcException(error);
+    }
+  }
+}
